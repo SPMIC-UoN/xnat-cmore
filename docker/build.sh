a8c6ff70047278ca96697e60ff471a01f70a2d32
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-06 13:20:51 +0000

    Add docker wrapper code for b0/t2* mapping

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp Dockerfile.in Dockerfile
+python ../cmd2label.py ukat_t2star_cmd.json ukat_b0_cmd.json >> Dockerfile
+
+tag=0.0.1
+docker build -t martincraig/xnat-cmore .
+docker tag martincraig/xnat-cmore martincraig/xnat-cmore:$tag 
+#docker push martincraig/xnat-cmore:$tag
