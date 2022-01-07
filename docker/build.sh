bc701653ede7a3d83fbbb0352a2cb01115a9d17a
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-07 15:01:25 +0000

    Push docker image now things are a bit more stable

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,4 @@ python ../cmd2label.py ukat_t2star_cmd.json ukat_b0_cmd.json cmore_preproc_cmd.j
 tag=0.0.1
 docker build -t martincraig/xnat-cmore .
 docker tag martincraig/xnat-cmore martincraig/xnat-cmore:$tag 
-#docker push martincraig/xnat-cmore:$tag
+docker push martincraig/xnat-cmore:$tag
