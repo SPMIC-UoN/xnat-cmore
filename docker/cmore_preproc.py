555848a6eb7f24f305ce0fd37fc3feee0b270117
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-07 14:58:56 +0000

    Allow dcm2niix flags to be set and remove empty output dirs

--- docker/cmore_preproc.py
+++ docker/cmore_preproc.py
@@ -17,20 +17,29 @@ from ukat.mapping.b0 import B0
 
 indir = sys.argv[1]
 outdir = sys.argv[2]
-t2star_method = sys.argv[3]
+if len(sys.argv) > 3:
+    dcm2niix_args = sys.argv[3]
+else:
+    dcm2niix_args = "-m n -f %%n_%%p_%%q -z y"
+if len(sys.argv) > 4:
+    t2star_method = sys.argv[4]
+else:
+    t2star_method = "all"
 
 # Load input data
 print("Loading from %s" % indir)
-print("Produciung output in %s" % outdir)
+print("Producing output in %s" % outdir)
 
 for fname in os.listdir(indir):
     print(fname)
 
+# Do nifti conversion
 os.makedirs("%s/nifti" % outdir, exist_ok=True, mode=0o777)
-cmd = "dcm2niix -o %s/nifti -f %%n_%%p_%%q -z y %s" % (outdir, indir)
+cmd = "dcm2niix -o %s/nifti %s %s" % (outdir, dcm2niix_args, indir)
 print(cmd)
 os.system(cmd)
 
+# Generate T2* map if we have relevant data
 os.makedirs("%s/t2star_in" % outdir)
 os.system("cp %s/nifti/*T2star* %s/t2star_in" % (outdir, outdir))
 os.makedirs("%s/t2star" % outdir)
@@ -38,12 +47,17 @@ cmd = "python ukat_t2star.py %s/t2star_in %s/t2star %s" % (outdir, outdir, t2sta
 print(cmd)
 os.system(cmd)
 
+# Generate B0 map if we have relevant data
 os.makedirs("%s/b0map" % outdir)
 cmd = "python ukat_b0.py %s/t2star_in %s/b0map" % (outdir, outdir)
 print(cmd)
 os.system(cmd)
 
+# Make sure we don't upload empty resource catalogs for irrelevant outputs
+for subdir in ("b0map", "t2star"):
+    d = os.path.join(outdir, subdir)
+    if not os.listdir(d):
+        os.rmdir(d)
+
 print("DONE")
-print("%s" % os.listdir(outdir))
-print("%s" % os.listdir("%s/nifti" % outdir))
 
