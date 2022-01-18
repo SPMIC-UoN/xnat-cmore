35c83a61d4d03070f3228ac285036e287a1385ef
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-18 14:09:19 +0000

    Tag as latest image

--- docker/build.sh
+++ docker/build.sh
@@ -7,3 +7,4 @@ tag=0.0.1
 docker build -t martincraig/xnat-cmore .
 docker tag martincraig/xnat-cmore martincraig/xnat-cmore:$tag 
 docker push martincraig/xnat-cmore:$tag
+docker push martincraig/xnat-cmore:latest
