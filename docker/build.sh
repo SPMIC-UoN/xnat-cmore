124a1834f197cd52faae370ad81b7dfab2f11606
Martin Craig <martin.craig@nottingham.ac.uk>
2022-03-04 15:35:23 +0000

    Bump version

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 cp Dockerfile.in Dockerfile
 python scripts/cmd2label.py cmore_t2star_cmd.json cmore_b0_cmd.json cmore_preproc_cmd.json >> Dockerfile
 
-tag=0.0.1
+tag=0.0.2
 docker build -t martincraig/xnat-cmore .
 docker tag martincraig/xnat-cmore martincraig/xnat-cmore:$tag 
 docker push martincraig/xnat-cmore:$tag
