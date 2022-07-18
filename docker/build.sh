0293ce583bf72d73671616f82a81b087aa02475b
Martin Craig <martin.craig@nottingham.ac.uk>
2022-07-18 09:34:09 +0100

    Have removed single preprocessing commands

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp Dockerfile.in Dockerfile
-python scripts/cmd2label.py cmore_t2star_cmd.json cmore_b0_cmd.json cmore_preproc_cmd.json >> Dockerfile
+python scripts/cmd2label.py cmore_preproc_cmd.json >> Dockerfile
 
 tag=0.0.2
 docker build -t martincraig/xnat-cmore .
